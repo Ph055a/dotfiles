b034cd937b4dd4cf27e0633c9d506b9239740e91
Ph055a <Ph055a@protonmail.com>
2018-02-26 23:07:53 -0500

    Added incomplete install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Update and upgrade
+sudo apt update && sudo apt upgrade -y
+# Install common software
+sudo apt install -y git firefox htop nethogs golang lua5.3 awesome compton feh gnome-terminal zeal
+
