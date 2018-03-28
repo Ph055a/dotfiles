53deecc1bc3628c0950c4c4506541ed84a8bfb4c
Ph055a <ph055a@protonmail.com>
2018-03-28 16:40:36 +1100

    Added docker and Golang scripts

---
+++ go_gets.sh
@@ -0,0 +1,13 @@
+# Go get all my common go repo's
+echo "Getting github.com/pilu/fresh"
+go get -u github.com/pilu/fresh
+echo "Getting github.com/PuerkitoBio/goquery"
+go get -u github.com/PuerkitoBio/goquery
+echo "Getting github.com/gorilla/mux"
+go get -u github.com/gorilla/mux
+echo "Getting github.com/GoesToEleven/golang-web-dev"
+go get -u github.com/GoesToEleven/golang-web-dev
+echo "Getting github.com/GoesToEleven/GolangTraining"
+go get -u github.com/GoesToEleven/GolangTraining
+echo "Getting github.com/gocolly/colly"
+go get -u github.com/gocolly/colly/
