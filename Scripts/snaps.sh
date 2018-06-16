cfab59a449987347e3222fbeb8e1cc6dd278a43c
Ph055a <ph055a@protonmail.com>
2018-06-16 15:11:28 +1000

    Added snaps install script

--- Scripts/snaps.sh
+++ Scripts/snaps.sh
@@ -1,11 +1,22 @@
 # Install snapd
 echo "Installing snapd"
 sudo apt update;sudo apt install snapd -y
-echo "Installing Micro"
-sudo snap install micro --classic
+
+# Utilities
 echo "Installing bitwarden"
 sudo snap install bitwarden
 echo "Installing Brave Browser"
 sudo snap intall brave
 echo "Installing keepasscx"
 sudo snap install keepasscx
+echo "Installing RocketChat Desktop"
+sudo snap install rocketchat-desktop
+
+# Development
+echo "Installing Micro Editor"
+sudo snap install micro --classic
+echo "Installing Atom Editor"
+sudo snap install atom --classic
+echo "Installing Zenkit"
+sudo snap install zenkit
+echo "Installing Gitkraken"
