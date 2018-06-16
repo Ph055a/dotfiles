a452114f865bc72e2e4040f6f7b0bc8a4b77e4d0
Ph055a <ph055a@protonmail.com>
2018-06-16 15:07:03 +1000

    Removed old install.sh file

---
+++ Scripts/snaps.sh
@@ -0,0 +1,11 @@
+# Install snapd
+echo "Installing snapd"
+sudo apt update;sudo apt install snapd -y
+echo "Installing Micro"
+sudo snap install micro --classic
+echo "Installing bitwarden"
+sudo snap install bitwarden
+echo "Installing Brave Browser"
+sudo snap intall brave
+echo "Installing keepasscx"
+sudo snap install keepasscx
