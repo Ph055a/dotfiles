49dfe1dd3a04bad52ccd19b7cd96b03172a0f0c7
Ph055a <ph055a@protonmail.com>
2018-07-14 13:25:28 +1000

    Added i3blocks.conf

---
+++ gems.sh
@@ -0,0 +1,35 @@
+# Gem install script
+echo "Installing Bundler"
+sudo gem install bundler
+echo "Installing Capybara"
+sudo gem install capybara
+echo "Installing CSV"
+sudo gem install csv
+echo "Installing Cucumber"
+sudo gem install cucumber
+echo "Installing Domain_name"
+sudo gem install domain_name
+echo "Installing Gherkin"
+sudo gem install gherkin
+echo "Installing IPaddr"
+sudo gem install ipaddr
+echo "Installing JSON"
+sudo gem install json
+echo "Installing Nokogiri"
+sudo gem install nokogiri
+echo "Installing Rspec"
+sudo gem install rspec
+echo "Installing Selenium-webdriver"
+sudo gem install selenium-webdriver
+echo "Installing Spreadshhet"
+sudo gem install spreadsheet
+echo "Installing Vagrant"
+sudo gem install vagrant
+echo "Installing Watir"
+sudo gem install watir
+echo "Installing Watir-webdriver"
+sudo gem install watir-webdriver
+echo "Installing Websocket-extensions"
+sudo gem install websocket-extensions
+echo "Installing Xpath"
+sudo gem install xpath
