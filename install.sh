b41b8a6d32609be24210588616bd0076cf08b27c
Ph055a <ph055a@protonmail.com>
2019-09-08 19:23:56 +0700

    Added basic setup script

---
+++ install.sh
@@ -0,0 +1,35 @@
+sudo apt update; sudo apt upgrade -y
+
+sudo apt install -y \
+vlc \
+gparted \
+transmission-qt \
+clamav \
+ruby-dev \
+git \
+zsh \
+i3 \
+rofi \
+feh \
+wget \
+curl \
+httpie \
+arc-theme
+
+i="sudo snap install"
+
+$i bitwarden
+$i brave
+$i gotop
+$i libreoffice
+$i micro --classic
+$i poddr
+$i powershell-preview  --classic
+$i rocketchat-desktop
+$i shotcut --classic
+$i signal-desktop
+$i telegram-desktop
+$i whalebird
+$i keepassxc
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
